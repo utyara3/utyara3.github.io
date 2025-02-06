0c04d505cae2faa7d8becb9a30f69255c191b733
utyara3 <prorok2018pro@gmail.com>
2025-02-07 00:39:53 +0300

    Новая версия сайта \O/

---
+++ script.js
@@ -0,0 +1,88 @@
+function loadContent(url) {
+    const request = new XMLHttpRequest()
+    request.onload = function() {
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(this.responseText, 'text/html')
+      const newContent = doc.getElementById('main-content')
+      if (newContent) {
+        document.getElementById('main-content').innerHTML = newContent.innerHTML
+    }
+    }
+    request.open('GET', url)
+    request.send()
+    window.scrollTo(0, 0);
+  }
+
+
+document.addEventListener('click', (event) => {
+    const loadElement = event.target.closest('.load')
+    const active_li = document.querySelector("#active")
+    if (loadElement) {
+        if (event.target.id != 'active') {
+            if (active_li) {
+                active_li.removeAttribute('id')
+            }
+            if (event.target.tagName == "LI") {
+                event.target.id = 'active'
+            } else {
+                event.target.parentElement.id = 'active'
+            }
+        }
+        const page = loadElement.classList[1]; 
+        loadContent(`${page}.html`);
+        document.getElementById("sidebar").classList.toggle("open");
+
+    }
+    
+}, true);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+    if (window.innerWidth <= 768) {
+        sidebar.classList.remove('open');
+    } else {
+        sidebar.classList.add('open');
+    };
+});
+
+
+window.addEventListener('resize', function() {
+    const sidebar = document.getElementById('sidebar');
+    if (window.innerWidth > 768) {
+        sidebar.classList.add('open');
+    } else {
+        sidebar.classList.remove('open');
+    };
+});
+
+document.querySelectorAll(".menu-but").forEach(element => {
+        element.addEventListener("click", () =>{
+        document.getElementById("sidebar").classList.toggle("open");
+    });
+});
+
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      document.querySelector('.btn-up').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
